31c62a13efb1c31ab24bea09bcda1de6941fdf10
linh4 <linh44h@gmail.com>
2018-12-14 23:59:11 -0500

    added auth

--- app.js
+++ app.js
@@ -1,16 +1,72 @@
-const express     = require('express'),
-      app         = express(),
-      bodyParser  = require('body-parser'),
-      mongoose    = require('mongoose'),
-      Home        = require('./models/home'),
-      Comment     = require('./models/comment')
-      seedDB      = require('./seeds')
+const express       = require('express'),
+      app           = express(),
+      bodyParser    = require('body-parser'),
+      passport      = require('passport'),
+      LocalStrategy = require('passport-local'),
+      mongoose      = require('mongoose'),
+      Home          = require('./models/home'),
+      Comment       = require('./models/comment'),
+      User          = require('./models/user')
+      seedDB        = require('./seeds')
 
 mongoose.connect("mongodb://localhost:27017/homes", { useNewUrlParser: true })
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 seedDB()
+
+// Passport configuration
+app.use(require('express-session')({
+  secret: 'this is a secret',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+
+// AUTH ROUTES
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const newUser = new User({username: req.body.username})
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      console.log(err)
+      return res.render('/register');
+    }
+    passport.authenticate('local')(req, res, () => {
+      res.redirect('/homes');
+    })
+  })
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/homes',
+  failureRedirect: '/login'
+}), (req, res) => {
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/homes');
+});
+
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/login');
+}
 
 app.get('/', (req, res) => {
   res.render('homepage');
@@ -55,7 +111,9 @@ app.get('/homes/:id', (req, res) => {
   })
 });
 
-app.get('/homes/:id/comments/new', (req, res) => {
+// COMMENTS ROUTES
+
+app.get('/homes/:id/comments/new', isLoggedIn, (req, res) => {
   Home.findById(req.params.id, (err, foundHome) => {
     if (err) {
       console.log(err)
@@ -65,7 +123,7 @@ app.get('/homes/:id/comments/new', (req, res) => {
   })
 });
 
-app.post('/homes/:id/comments', (req, res) => {
+app.post('/homes/:id/comments', isLoggedIn, (req, res) => {
   Home.findById(req.params.id, (err, home) => {
     if (err) {
       console.log(err)
